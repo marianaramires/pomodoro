a5f5ac00a911b5ff02d0304bb923fbb86fcd9b96
marianaramires <82910718+marianaramires@users.noreply.github.com>
2021-07-03 17:59:44 -0300

    botões - e +

--- pomodoro.js
+++ pomodoro.js
@@ -1,16 +1,42 @@
-var comecar = document.getElementById('comecar');
-var pausar = document.getElementById('pausar');
-var reiniciar = document.getElementById('reiniciar');
+var comecar = document.getElementById('comecar')
+var pausar = document.getElementById('pausar')
+var reiniciar = document.getElementById('reiniciar')
 
-var fm = document.getElementById('f_min');
-var fs = document.getElementById('f_sec');
+var f_menos = document.getElementById('f_menos')
+var f_mais = document.getElementById('f_mais')
+var p_menos = document.getElementById('p_menos')
+var p_mais = document.getElementById('p_mais')
 
-var pm = document.getElementById('p_min');
-var ps = document.getElementById('p_sec');
+var fm = document.getElementById('f_min')
+var fs = document.getElementById('f_sec')
 
-var comecarTimerFoco;
+var pm = document.getElementById('p_min')
+var ps = document.getElementById('p_sec')
+
+var cont = document.getElementById('cont')
+
+var comecarTimerFoco
 var comecarTimerPausa
 
+// botões de diminuir minutos
+f_menos.addEventListener('click', function(){
+    fm.innerText--;
+})
+
+p_menos.addEventListener('click', function(){
+    pm.innerText--;
+})
+
+// botões de aumentar minutos
+f_mais.addEventListener('click', function(){
+    fm.innerText++;
+})
+
+p_mais.addEventListener('click', function(){
+    pm.innerText++;
+})
+
+// botão começar
 comecar.addEventListener('click', function(){
     if(comecarTimerFoco === undefined && comecarTimerPausa === undefined) {
         comecarTimerFoco = setInterval(timer_foco, 1000)
@@ -20,12 +46,14 @@ comecar.addEventListener('click', function(){
     }
 })
 
+// botão pausar
 pausar.addEventListener('click', function(){
     pararIntervalo()
     comecarTimerFoco = undefined;
     comecarTimerPausa = undefined
 })
 
+// botão reiniciar
 reiniciar.addEventListener('click', function(){
     fm.innerText = 25;
     fs.innerText = 00;
@@ -49,6 +77,13 @@ function timer_foco() {
     if(fm.innerText == 0 && fs.innerText == 1) {
         document.getElementById('audio').play();
     }
+
+    // cont sessões
+    if(fm.innerText == 0 && fs.innerText == 0 && pm.innerText == 0 && ps.innerText == 0){
+        fm.innerText = 25;
+        pm.innerText = 5;
+        cont.innerText++;
+    }
 }
 
 // timer de pausa
@@ -62,16 +97,6 @@ function timer_pausa() {
         }
     }
 
-    // cont sessões
-    if(fm.innerText == 0 && fs.innerText == 0 && pm.innerText == 0 && ps.innerText == 0){
-        fm.innerText = 25;
-        fs.innerText = 00;
-        pm.innerText = 5;
-        ps.innerText = 00;
-
-        document.getElementById('cont').innerText++;
-    }
-
     if(pm.innerText == 0 && ps.innerText == 1) {
         document.getElementById('audio').play();
     }
@@ -82,4 +107,3 @@ function pararIntervalo() {
     clearInterval(comecarTimerFoco)
     clearInterval(comecarTimerPausa)
 }
-
